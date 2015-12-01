0b14500cff033260c9ab6e1309ffbf44664f969d
James O'Neal <jamesponeal@gmail.com>
2015-12-01 17:39:11 -0600

    update to variables-methods.rb

--- week-4/variables-methods.rb
+++ week-4/variables-methods.rb
@@ -23,24 +23,37 @@ puts "~" * 10
 # We were asked to answer the following questions:
 
 # 1. ~ How do you define a local variable?
-# A local variable is a name assigned to some string or value so that it can be stored and recalled later to use in another part of a program.  They are very useful if a certain string or value is to be used more than once.
+# A local variable is a name assigned to some string or value so that it can be
+# stored and recalled later to use in another part of a program.  They are very
+# useful if a certain string or value is to be used more than once.
 
 # 2. ~ How do you define a method?
-# As Chris Pine would say, a method is a thing that does stuff.  To make an analogy to the English language, a method is like a verb.  It is an action that is done on an object (a noun).
+# As Chris Pine would say, a method is a thing that does stuff.  To make an analogy
+# to the English language, a method is like a verb.  It is an action that is done
+# on an object (a noun).
 
 # 3. ~ What is the difference between a local variable and a method?
-# The two are very different.  A local variable is just a name assigned to a string or value.  A method is an action.  Methods can take variables as parameters, and can manipulate variables.
+# The two are very different.  A local variable is just a name assigned to a string
+# or value.  A method is an action.  Methods can take variables as parameters, and
+# can manipulate variables.
 
 # 4. ~ How do you run a ruby program from the command line?
-# To run a ruby program from the command line, type "ruby filename.rb", where "filename" is the name of the file you want to run.
+# To run a ruby program from the command line, type "ruby filename.rb", where
+# "filename" is the name of the file you want to run.
 
 # 5. ~ How do you run an RSpec file from the command line?
-# To run an rspec file from the command line, type "rspec filename.rb", where "filename" is the name of the rspec file you want to run. In the examples we have done so far "_rspec" was included at the end of the filename.
+# To run an rspec file from the command line, type "rspec filename.rb", where
+# "filename" is the name of the rspec file you want to run. In the examples we
+# have done so far "_rspec" was included at the end of the filename.
 
 # 6. ~ What was confusing about this material? What made sense?
-# The thing that I got hung up on was trying to run my first rspec file, that was partly because I screwed up and copied the wrong file from the GitHub repo into my local file, but also because I felt the instructions were confusing.  After I got it, then it was easy.
+# The thing that I got hung up on was trying to run my first rspec file, that
+# was partly because I screwed up and copied the wrong file from the GitHub repo
+# into my local file, but also because I felt the instructions were confusing.
+# After I got it, then it was easy.
 
-# Finally, we were asked to provide a link to our solution files for 4.3.  I have copied and pasted mine below:
+# Finally, we were asked to provide a link to our solution files for 4.3.
+# I have copied and pasted mine below:
 
 # Return A Formatted Address:
 # https://github.com/jamesponeal/phase-0/blob/master/week-4/address/my_solution.rb
