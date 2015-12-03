86c5a3fda18db88bc20bcf748ee8694dd2502365
James O'Neal <jamesponeal@gmail.com>
2015-12-02 18:06:08 -0600

    add shortest string files

---
+++ week-4/shortest-string/my_solution.rb
@@ -0,0 +1,29 @@
+# Shortest String
+
+# I worked on this challenge [by myself, with: ].
+
+# shortest_string is a method that takes an array of strings as its input
+# and returns the shortest string
+#
+# +list_of_words+ is an array of strings
+# shortest_string(array) should return the shortest string in the +list_of_words+
+#
+# If +list_of_words+ is empty the method should return nil
+
+#Your Solution Below
+def shortest_string(array)
+  test_array = []
+  test_array.push(array[0])
+  array.each do |test|
+    if test.length < test_array[0].length
+      test_array.pop
+      test_array.push(test)
+    end
+  end
+  puts test_array[0]
+  return test_array[0]
+end
+
+
+#list_of_words = ["super-zebra", "aardvark", "rhinocerous", "gorilla", "monkey"]
+#shortest_string(list_of_words)
