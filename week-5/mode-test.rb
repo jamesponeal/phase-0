9508f04e3c8b1d61e5fea15967b48102464bb80d
James O'Neal <jamesponeal@gmail.com>
2015-12-10 11:39:20 -0600

    add calculate mode files

---
+++ week-5/mode-test.rb
@@ -0,0 +1,37 @@
+
+
+array1 = ["dog", "cat", "cat", "horse", "horse", "cat", "horse"]
+
+def mode(array)
+  hash1 = Hash[array.map {|x| [x, array.count(x)]}]
+  count = 1
+  hash1.each_value do |y|
+    if y > count
+      count = y
+    end
+  end
+  final_array = []
+  hash1.each do |key, value|
+    if value == count
+      final_array.push(key)
+    end
+  end
+  p final_array
+end
+
+
+
+# def mode(array)
+#  hash2 = {}
+#  newer_array = []
+#  array.each do |value|
+#    if hash2.has_key?(value)
+#      hash2[value] += 1
+#    else
+#      hash2[value] = 1
+#    end
+#  end
+#  p hash2
+# end
+
+mode(array1)
