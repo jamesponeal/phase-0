6b037efb5ca089874f90d0572b8a048c25cd8058
James O'Neal <jamesponeal@gmail.com>
2015-12-23 22:12:46 -0600

    add group project files

---
+++ week-7/group-project/group_project_solution.js
@@ -0,0 +1,12 @@
+// Group 12
+// Person 1: Jim O'Neal
+
+// Ok, I'm going to do my best to follow the instructions and write my user stories without using any code language at all, although the instructions do say that I can use the name of the functions that the tests intend for us to write.  Here it goes:
+
+
+// Function name 'sum':  As a user, I want a way to add up a bunch of numbers.  The bunch of numbers could be any quantity of numbers.
+
+// Function name 'mean':  As a user, I want a way to find the average of a bunch of numbers.  The bunch of numbers could be any quantity of numbers.
+
+// Function name 'median':  As a user, I want a way to find the median of a bunch of numbers.  The bunch of numbers could be any quantity of numbers.
+
