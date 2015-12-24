6db6f29d6ca7afa5f2c0daa88b73bd45e7cd000a
James O'Neal <jamesponeal@gmail.com>
2015-12-23 22:24:26 -0600

    update to group_project file

--- week-7/group-project/group_project_solution.js
+++ week-7/group-project/group_project_solution.js
@@ -1,12 +1,18 @@
 // Group 12
 // Person 1: Jim O'Neal
 
-// Ok, I'm going to do my best to follow the instructions and write my user stories without using any code language at all, although the instructions do say that I can use the name of the functions that the tests intend for us to write.  Here it goes:
+// Ok, I'm going to do my best to follow the instructions and write my user
+// stories without using any code language at all, although the instructions
+// do say that I can use the name of the functions that the tests intend for
+// us to write.  Here it goes:
 
+// As a user, I want to perform three different mathematical operations on a
+// list of numbers.  The list of numbers could be any quantity of numbers.  The
+// three things are three separate operations and don't need to be done all at
+// once.  I just want to be able to use whichever operation I want on any list
+// of numbers.  The three things I want to be able to do are:
+// 1) I want to add up (sum) the list of numbers.
+// 2) I want to find the average (mean) of the list of numbers.
+// 3) I want to find the middle value (median) of the list of numbers.
 
-// Function name 'sum':  As a user, I want a way to add up a bunch of numbers.  The bunch of numbers could be any quantity of numbers.
-
-// Function name 'mean':  As a user, I want a way to find the average of a bunch of numbers.  The bunch of numbers could be any quantity of numbers.
-
-// Function name 'median':  As a user, I want a way to find the median of a bunch of numbers.  The bunch of numbers could be any quantity of numbers.
 
