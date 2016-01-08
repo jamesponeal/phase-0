4b4993f827425a38b9f420e528255597c8518c1e
James O'Neal <jamesponeal@gmail.com>
2016-01-08 10:35:18 -0600

    update to week-8 test files

--- week-8/test.js
+++ week-8/test.js
@@ -1,45 +1,29 @@
 
-var students = ["Joseph", "Susan", "William", "Elizabeth"]
-
-var scores = [ [80, 70, 70, 100],
-               [85, 80, 90, 90],
-               [75, 70, 80, 75],
-               [100, 90, 95, 85] ]
 
 
-
-var gradebook = {};
-// gradebook.Joseph = {testscores: scores[0]};
-// gradebook.Susan = {testscores:scores[1]};
-// gradebook.William = {testscores:scores[2]};
-// gradebook.Elizabeth = {testscores:scores[3]};
+// var symbols = ["flag", "postage stamp", "logo", "signature", "seal"];
 
 
+// var target_index = symbols.index("signature");
 
-for (var x=0; x < students.length; x++) {
-  gradebook[students[x]]={testScores:scores[x]};
+// console.log(symbols[target_index]);
+
+
+// var counter = 3;
+
+// console.log(symbols[counter]);
+
+// console.log(symbols[3]);
+
+// console.log(symbols.3);
+
+
+
+var answerBank = {
+  optionOne: "Wrong!",
+  optionTwo: "Wrong!",
+  optionThree: "Correct",
 };
 
-console.log(gradebook);
-
-gradebook.addScore = function(name, score){
-  gradebook[name]["testScores"].push(score);
-};
-
-gradebook.addScore('Susan',100);
-
-console.log(gradebook);
-
-gradebook.getAverage = function(){
-
-};
-
-
-
-
-
-
-
-
-
+console.log(answerBank["optionThree"]);
 
---
+++ week-8/test.rb
@@ -0,0 +1,14 @@
+
+
+var symbols = ["flag", "postage stamp", "logo", "signature", "seal"]
+
+
+var target_index = symbols.index("signature")
+
+p symbols[target_index]
+
+
+var counter = 3
+
+symbols[counter]
+
