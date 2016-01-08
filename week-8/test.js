33e1714d488d41c3e1e6ef1e82a70aefb5d0cddc
James O'Neal <jamesponeal@gmail.com>
2016-01-08 15:06:53 -0600

    update tally votes file

--- week-8/test.js
+++ week-8/test.js
@@ -1,29 +1,77 @@
 
 
+var votes = {
+  "Alex": { president: "Bob", vicePresident: "Devin", secretary: "Gail", treasurer: "Kerry" },
+  "Bob": { president: "Mary", vicePresident: "Hermann", secretary: "Fred", treasurer: "Ivy" },
+  "Cindy": { president: "Cindy", vicePresident: "Hermann", secretary: "Bob", treasurer: "Bob" },
+  "Devin": { president: "Louise", vicePresident: "John", secretary: "Bob", treasurer: "Fred" },
+  "Ernest": { president: "Fred", vicePresident: "Hermann", secretary: "Fred", treasurer: "Ivy" },
+  "Fred": { president: "Louise", vicePresident: "Alex", secretary: "Ivy", treasurer: "Ivy" },
+  "Gail": { president: "Fred", vicePresident: "Alex", secretary: "Ivy", treasurer: "Bob" },
+  "Hermann": { president: "Ivy", vicePresident: "Kerry", secretary: "Fred", treasurer: "Ivy" },
+  "Ivy": { president: "Louise", vicePresident: "Hermann", secretary: "Fred", treasurer: "Gail" },
+  "John": { president: "Louise", vicePresident: "Hermann", secretary: "Fred", treasurer: "Kerry" },
+  "Kerry": { president: "Fred", vicePresident: "Mary", secretary: "Fred", treasurer: "Ivy" },
+  "Louise": { president: "Nate", vicePresident: "Alex", secretary: "Mary", treasurer: "Ivy" },
+  "Mary": { president: "Louise", vicePresident: "Oscar", secretary: "Nate", treasurer: "Ivy" },
+  "Nate": { president: "Oscar", vicePresident: "Hermann", secretary: "Fred", treasurer: "Tracy" },
+  "Oscar": { president: "Paulina", vicePresident: "Nate", secretary: "Fred", treasurer: "Ivy" },
+  "Paulina": { president: "Louise", vicePresident: "Bob", secretary: "Devin", treasurer: "Ivy" },
+  "Quintin": { president: "Fred", vicePresident: "Hermann", secretary: "Fred", treasurer: "Bob" },
+  "Romanda": { president: "Louise", vicePresident: "Steve", secretary: "Fred", treasurer: "Ivy" },
+  "Steve": { president: "Tracy", vicePresident: "Kerry", secretary: "Oscar", treasurer: "Xavier" },
+  "Tracy": { president: "Louise", vicePresident: "Hermann", secretary: "Fred", treasurer: "Ivy" },
+  "Ullyses": { president: "Louise", vicePresident: "Hermann", secretary: "Ivy", treasurer: "Bob" },
+  "Valorie": { president: "Wesley", vicePresident: "Bob", secretary: "Alex", treasurer: "Ivy" },
+  "Wesley": { president: "Bob", vicePresident: "Yvonne", secretary: "Valorie", treasurer: "Ivy" },
+  "Xavier": { president: "Steve", vicePresident: "Hermann", secretary: "Fred", treasurer: "Ivy" },
+  "Yvonne": { president: "Bob", vicePresident: "Zane", secretary: "Fred", treasurer: "Hermann" },
+  "Zane": { president: "Louise", vicePresident: "Hermann", secretary: "Fred", treasurer: "Mary" }
+}
 
-// var symbols = ["flag", "postage stamp", "logo", "signature", "seal"];
+var voteCount = {
+  president: {},
+  vicePresident: {},
+  secretary: {},
+  treasurer: {}
+}
+
+var officers = {
+  president: undefined,
+  vicePresident: undefined,
+  secretary: undefined,
+  treasurer: undefined
+}
 
 
-// var target_index = symbols.index("signature");
-
-// console.log(symbols[target_index]);
-
-
-// var counter = 3;
-
-// console.log(symbols[counter]);
-
-// console.log(symbols[3]);
-
-// console.log(symbols.3);
 
 
 
-var answerBank = {
-  optionOne: "Wrong!",
-  optionTwo: "Wrong!",
-  optionThree: "Correct",
-};
 
-console.log(answerBank["optionThree"]);
+for (candidates in voteCount){
+  for(voters in votes) {
+    name = votes[voters][candidates];
+    for(offices in voteCount) {
+      if (offices == candidates) {
+        if (voteCount[offices][name] >= 1) {
+        voteCount[offices][name] ++;
+        } else {
+          voteCount[offices][name] = 1;
+        }
+      }
+    }
+  }
+
+  numberChecker = 0;
+  for (people in voteCount[candidates]){
+    if (voteCount[candidates][people] > numberChecker) {
+     numberChecker = voteCount[candidates][people];
+      officers[candidates] = people;
+    }
+  }
+}
+
+
+
+
 
